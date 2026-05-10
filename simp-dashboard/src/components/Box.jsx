161e2e2ebf637539fb7b349b81d07afd779b7015
Phattaphon Taengngam <68110889+phattaphon12@users.noreply.github.com>
2026-05-11 00:13:07 +0700

    add overview box in monitor page

--- simp-dashboard/src/components/Box.jsx
+++ simp-dashboard/src/components/Box.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Box = ({ children, className = '' }) => {
   return (
-    <div className={`w-full max-w-[720px] min-w-0 border border-(--mtr-gray) rounded-[20px] shadow-[0px_4px_4px_rgba(0,0,0,0.25)] ${className}`}>
+    <div className={`w-full max-w-[720px] min-w-0 mx-auto border border-(--mtr-gray) rounded-[20px] shadow-[0px_4px_4px_rgba(0,0,0,0.25)] ${className}`}>
       {children}
     </div>
   );
--- simp-dashboard/src/components/Grid.jsx
+++ simp-dashboard/src/components/Grid.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Grid = ({ children, className = '' }) => {
   return (
-    <div className={`max-w-[1440px] grid grid-cols-12 gap-6 m-8 justify-items-stretch align-items-stretch ${className}`}>
+    <div className={`max-w-[1440px] grid grid-cols-2 md:grid-cols-4 xl:grid-cols-12 gap-6 m-8 justify-items-stretch align-items-stretch ${className}`}>
       {children}
     </div>
   );
